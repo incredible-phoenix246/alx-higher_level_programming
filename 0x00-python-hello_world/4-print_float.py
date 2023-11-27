228779592a5e534fa3a4c61d231cfab79696a57e
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-11-27 12:39:22 +0200

    mroe

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-# YOUR CODE GOES HERE
 print(f"Float: {number:.2f}\n")
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-# YOUR CODE GOES HERE
 print(f"{str * 3}\n{str[:9]}\n")
