002bd4d73ef5362caefc52ed13f8fa8a06591529
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-11-27 12:33:56 +0200

    new

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+number = 3.14159
+# YOUR CODE GOES HERE
+print(f"Float: {number:.2f}\n")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+# YOUR CODE GOES HERE
+print(f"{str * 3}\n{str[:9]}\n")
