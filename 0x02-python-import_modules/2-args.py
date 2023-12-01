e45da23eff0f071696dfac9c16e3d5b45253795a
Phoenix <ayomikuntemitope.araoye@gmail.com>
2023-12-01 02:39:45 +0200

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,7 @@ if __name__ == "__main__":
 
     num_args = len(argv) - 1
 
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        plural_s = 's' if num_args > 1 else ''
-        print("{} argument{}:".format(num_args, plural_s))
+    print("{} argument{}{}".format(num_args, 's' if num_args != 1 else '', ':' if num_args else '.'))
 
-        for i in range(1, num_args + 1):
-            print("{}: {}".format(i, argv[i]))
-
+    for i in range(1, num_args + 1):
+        print("{}: {}".format(i, argv[i]))
